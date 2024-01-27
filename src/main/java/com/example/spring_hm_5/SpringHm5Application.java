455d7ca6f9aabbe7ec0b2237661206a3e3c0bd33
Sergey Shkaev <shkaev.ss@mail.ru>
2024-01-27 16:15:59 +0300

    init & create README.md

---
+++ src/main/java/com/example/spring_hm_5/SpringHm5Application.java
@@ -0,0 +1,13 @@
+package com.example.spring_hm_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHm5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHm5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_hm_5/SpringHm5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_hm_5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHm5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
